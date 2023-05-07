1c91d46fa316a5ace1afc05d78ee4c47f4f07837
phinner <62483793+Phinner@users.noreply.github.com>
2023-05-07 15:16:50 +0200

    chore: Bump mammoth version to 1.3.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 dependencies {
     compileOnly(gradleApi())
     implementation("org.hjson:hjson:3.0.0")
-    implementation("net.kyori:mammoth:1.3.0")
+    implementation("net.kyori:mammoth:1.3.1")
     implementation(kotlin("stdlib-jdk8"))
 }
 
