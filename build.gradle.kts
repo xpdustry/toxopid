d514197d3923053ecf3c3acb4da3ff3e01f1ed04
phinner <62483793+phinner@users.noreply.github.com>
2024-06-01 17:21:25 +0200

    chore: Removed explicit kotlin plugin invocation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,5 @@
 plugins {
     id("com.diffplug.spotless") version "6.25.0"
-    kotlin("jvm") version "1.9.22"
     id("org.jetbrains.dokka") version "1.9.20"
     `java-gradle-plugin`
     id("com.gradle.plugin-publish") version "1.2.1"
@@ -22,6 +21,7 @@ repositories {
 dependencies {
     compileOnly(gradleApi())
     implementation("org.hjson:hjson:3.1.0")
+    implementation(kotlin("stdlib"))
 }
 
 signing {
