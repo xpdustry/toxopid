f7adfe545909d9a2e0798eeb3525093579acff52
Finley Experience <62483793+phinner@users.noreply.github.com>
2026-01-21 23:42:10 +0100

    chore: Prepare next version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "com.xpdustry"
-version = "4.1.3" + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
+version = "4.2.0" + if (indraGit.headTag() == null) "-SNAPSHOT" else ""
 description = "Gradle plugin for building and testing mindustry mods/plugins."
 
 repositories {
